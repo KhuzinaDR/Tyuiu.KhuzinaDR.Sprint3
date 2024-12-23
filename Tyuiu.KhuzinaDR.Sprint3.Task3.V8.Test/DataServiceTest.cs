e26ef851340c39bb9dccfd9632c9c3badf56734c
angeldrip9919 <i61angel>
2024-12-23 18:15:08 +0500

    Task3

---
+++ Tyuiu.KhuzinaDR.Sprint3.Task3.V8.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.KhuzinaDR.Sprint3.Task3.V8.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
