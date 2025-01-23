4aa4ec38fb1541600442488a33892b4d8475c6f6
angeldrip9919 <i61angel>
2025-01-24 00:13:43 +0500

    Task7

---
+++ Tyuiu.KhuzinaDR.Sprint3.Task7.V29/Program.cs
@@ -0,0 +1,52 @@
+﻿using Tyuiu.KhuzinaDR.Sprint3.Task7.V29.Lib;
+
+namespace Tyuiu.KhuzinaDR.Sprint3.Task7.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнила: Хузина Д. Р. | СМАРТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнила: Хузина Диана Ранидовна | СМАРТб-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Напишите программу, которая выводит таблицу значений функции            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine("Начало отрезка = " + startValue);
+            Console.WriteLine("Конец отрезка = " + stopValue);
+
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+            double[] valueArray;
+            valueArray = new double[len];
+
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("+----------+-----------+");
+            Console.WriteLine("|    X     |   f(x)    |");
+            Console.WriteLine("+----------+-----------+");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("|{0,5:d}     |  {1, 5:f2}    |", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+----------+-----------+");
+            Console.ReadKey();
+        }
+    }
+}
