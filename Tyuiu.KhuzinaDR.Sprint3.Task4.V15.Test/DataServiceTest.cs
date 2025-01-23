16d649efe3f5664704b6aaafc4f9b1245438263c
angeldrip9919 <i61angel>
2025-01-23 21:04:28 +0500

    Task4

---
+++ Tyuiu.KhuzinaDR.Sprint3.Task4.V15.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KhuzinaDR.Sprint3.Task4.V15.Lib;
+
+namespace Tyuiu.KhuzinaDR.Sprint3.Task4.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            double res = ds.Calculate(-5, 5);
+            Assert.AreEqual(res, 623.499);
+        }
+    }
+}
